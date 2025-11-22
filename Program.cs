87e6f7aee55971edcef32feda920d119aba99571
mojtaba.n <mojtaba.n@behinrahkar.com>
2025-11-23 00:42:31 +0330

    feat: add middleware

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var app = builder.Build();
 
 app.MapGet("/", (IGreeting greeting) => greeting.GetGreeting());
 
+app.UseLogging();
+
 app.Run();
 
 interface IGreeting
@@ -27,5 +29,32 @@ public static class GreetingServiceBuilderExtension
         services.AddScoped<IGreeting, Greeting>();
 
         return services;
+    }
+}
+
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.Headers.Append("my-header", "my-value");
+
+        await _next(context);
+
+        Console.WriteLine(context.Request.Path);
+    }
+}
+
+public static class LoggingMiddlewareExtension
+{
+    public static IApplicationBuilder UseLogging(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<LoggingMiddleware>();
     }
 }
