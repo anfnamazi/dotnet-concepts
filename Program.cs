26880144a929640938f97e9d3670bd7f886ebec8
mojtaba.n <mojtaba.n@behinrahkar.com>
2025-11-23 00:14:50 +0330

    feat: add GreetingServiceBuilder extension

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGreeting();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (IGreeting greeting) => greeting.GetGreeting());
 
 app.Run();
+
+interface IGreeting
+{
+    public string GetGreeting();
+}
+
+class Greeting : IGreeting
+{
+    public string GetGreeting()
+    {
+        return "Hello!";
+    }
+}
+
+public static class GreetingServiceBuilderExtension
+{
+    public static IServiceCollection AddGreeting(this IServiceCollection services)
+    {
+        services.AddScoped<IGreeting, Greeting>();
+
+        return services;
+    }
+}
